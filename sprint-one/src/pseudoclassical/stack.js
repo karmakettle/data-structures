d629e16507cfd621e3ed190505719d78a15cd56e
MKS-Student <mks.student@hackreactor.com>
2015-03-05 15:35:53 -0600

    Completed stack and queue pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.len = 0;
+};
+
+Stack.prototype.push = function(value){
+	this[this.len] = value;
+	this.len++;
+};
+
+Stack.prototype.pop = function(){
+	this.len && this.len--;
+	var result = this[this.len];
+	delete this[this.len];
+	return result;
+};
+
+Stack.prototype.size = function(){
+	return this.len;
 };
 
 
