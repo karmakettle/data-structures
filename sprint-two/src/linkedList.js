42cff5b48a5499e82501394a9990644385fb364f
MKS-Student <mks.student@hackreactor.com>
2015-03-05 20:13:55 -0600

    Working on linked list, about to implement contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if (!list.head) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    if (list.head.next) {
+      list.head = list.head.next;
+    }
+    return result;
   };
 
   list.contains = function(target){
+    var node = list.head;
+    while (node.next) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
   };
 
   return list;
