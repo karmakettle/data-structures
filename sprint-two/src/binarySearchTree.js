90817c7e4aa873770d3d47385ee6ba247f45f920
MKS-Student <mks.student@hackreactor.com>
2015-03-06 20:37:19 -0600

    Basic code structure established

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,25 @@
 var BinarySearchTree = function(value){
+	var tree = Object.create(treeMethods);
+	this.value = value;
+	this.left = undefined;
+	this.right = undefined;
+
+	return tree;
 };
 
+BinarySearchTree.treeMethods = {};
+
+treeMethods.insert = function(value) {
+
+};
+
+treeMethods.contains = function(target) {
+
+};
+
+treeMethods.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
